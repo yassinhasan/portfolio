46210f40a973874def038d9541db4de3119f4a39
yassinhasan <figo781@gmail.com>
2024-03-02 14:40:15 +0300

    first commit4

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let menu_btn = document.querySelector(".menu-btn");
 let inner_links = document.querySelector(".inner-links");
+let fa_moon = document.querySelector(".fa-moon");
 menu_btn.addEventListener("click",()=>{
     inner_links.classList.toggle("show")
+})
+fa_moon.addEventListener("click",()=>{
+    document.body.classList.toggle("light")
 })
